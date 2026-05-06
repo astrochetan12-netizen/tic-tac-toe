50871f6d0be7d3d08ea4de9c5855130d20c553a1
astrochetan12-netizen <astrochetan12@gmail.com>
2026-05-06 16:25:27 +0530

    UC5: Validate user move

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,1 +1,35 @@
+public class TicTacToe {
 
+    static char[][] board = {
+
+            {'-', '-', '-'},
+            {'-', 'X', '-'},
+            {'-', '-', '-'}
+    };
+
+    public static void main(String[] args) {
+
+        int row = 1;
+        int col = 1;
+
+        System.out.println(isValidMove(row, col));
+    }
+
+    // Validate move
+    static boolean isValidMove(int row, int col) {
+
+        // Check boundaries
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+
+            return false;
+        }
+
+        // Check if cell is empty
+        if (board[row][col] != '-') {
+
+            return false;
+        }
+
+        return true;
+    }
+}
