a38a9e98a915db85fde3018a677a91050b6d8ee4
astrochetan12-netizen <astrochetan12@gmail.com>
2026-05-06 16:38:43 +0530

    UC10: Add draw detection

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,57 +2,32 @@ public class TicTacToe {
 
     static char[][] board = {
 
-            {'X', 'X', 'X'},
-            {'O', '-', 'O'},
-            {'-', '-', '-'}
+            {'X', 'O', 'X'},
+            {'X', 'O', 'O'},
+            {'O', 'X', 'X'}
     };
 
     public static void main(String[] args) {
 
-        System.out.println(hasWon('X'));
+        System.out.println(isDraw());
     }
 
-    // Check win condition
-    static boolean hasWon(char symbol) {
+    // Check draw condition
+    static boolean isDraw() {
 
-        // Check rows
-        for (int row = 0; row < 3; row++) {
+        // Traverse board
+        for (int r = 0; r < 3; r++) {
 
-            if (board[row][0] == symbol &&
-                board[row][1] == symbol &&
-                board[row][2] == symbol) {
+            for (int c = 0; c < 3; c++) {
 
-                return true;
+                // Empty cell found
+                if (board[r][c] == '-') {
+
+                    return false;
+                }
             }
         }
 
-        // Check columns
-        for (int col = 0; col < 3; col++) {
-
-            if (board[0][col] == symbol &&
-                board[1][col] == symbol &&
-                board[2][col] == symbol) {
-
-                return true;
-            }
-        }
-
-        // Check main diagonal
-        if (board[0][0] == symbol &&
-            board[1][1] == symbol &&
-            board[2][2] == symbol) {
-
-            return true;
-        }
-
-        // Check opposite diagonal
-        if (board[0][2] == symbol &&
-            board[1][1] == symbol &&
-            board[2][0] == symbol) {
-
-            return true;
-        }
-
-        return false;
+        return true;
     }
 }
