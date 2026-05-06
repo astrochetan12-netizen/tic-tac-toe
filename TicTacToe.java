d087d602fb903771bd0bbd5015cefb093eccf8ef
astrochetan12-netizen <astrochetan12@gmail.com>
2026-05-06 16:21:10 +0530

    UC4: Convert slot number to board indices

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,1 +1,23 @@
+public class TicTacToe {
 
+    public static void main(String[] args) {
+
+        int slot = 7;
+
+        System.out.println("Row: " + getRowFromSlot(slot));
+
+        System.out.println("Column: " + getColFromSlot(slot));
+    }
+
+    // Convert slot number to row index
+    static int getRowFromSlot(int slot) {
+
+        return (slot - 1) / 3;
+    }
+
+    // Convert slot number to column index
+    static int getColFromSlot(int slot) {
+
+        return (slot - 1) % 3;
+    }
+}
